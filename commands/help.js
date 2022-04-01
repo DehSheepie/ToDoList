a1a4c8be3385d3ef0dc15c08cdf7493519c9707e
Shep <shep@cyberdragon.online>
2022-04-01 23:43:11 +0100

    Made the help message more useful
    Most of the functionality described isn't implemented yet but at least I know what I'm working towards at the very least

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,16 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
     .setTitle("Help!")
     .setColor(0xff0000)
-    .setDescription("I'm going to try to put useful info on how to use this bot here!");
+    .setDescription("I'm going to try to put useful info on how to use this bot here! \
+    Below are some ~~hopefully~~ useful command descriptions\!")
+    .addFields(
+      {name: '\u200b', value: '\u200b'},
+      {name: "__**tdl-d/tdl**__", value:"**Your daily to do list!** Use this command to have a look at what you need to do today.", inline: true},
+      {name: "__**tdl-l**__", value:"**Your longterm to do list!** Use this command to check in with your dreams and aspirations ^-^.", inline: true},
+      {name: '\u200b', value: '\u200b'},
+      {name: "__**tdl-d+/tdl+**__", value:"Need to add a todo? Use this command to **add a todo to your daily list**.", inline: true},
+      {name: "__**tdl-l+**__", value:"Got some longterm goals and aspirations to jot down? Use this command to **add to your longterm todo list**.", inline:true},
+    )
 
     // sends the embed
     message.channel.send({embeds: [embed]});
